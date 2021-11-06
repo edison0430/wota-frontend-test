bee07ac31b9b2868a15ae398a5d56d13ac3e0055
Edison Wang <edison763092@gmail.com>
2021-11-06 21:26:28 +0800

    build: add quasar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { Quasar } from 'quasar';
+import quasarLang from 'quasar/lang/zh-TW';
 
-createApp(App).mount('#app')
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
+
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.use(Quasar, {
+  plugins: {},
+  lang: quasarLang,
+});
+
+app.mount('#app');
